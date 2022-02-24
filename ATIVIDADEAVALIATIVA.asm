32d4870bb1cf525020ac58514f33442a432fe5d5
Antônio Elvis Pinheiro <52458608+Elvis0032@users.noreply.github.com>
2022-02-24 18:52:54 -0300

    Update ATIVIDADEAVALIATIVA.asm

--- ATIVIDADEAVALIATIVA.asm
+++ ATIVIDADEAVALIATIVA.asm
@@ -5,14 +5,84 @@
 	num2: .asciiz "Inoforme o Segundo Número: "
 	result: .asciiz "O resultado do calculo é: "
 
+.macro soma 
+
+la $a0, num1
+li $v0, 4
+syscall 
+li $v0, 5
+syscall
+move $t1, $v0
+
+
+la $a0, num2
+li $v0, 4
+syscall
+li $v0, 5
+syscall
+move $t2, $v0
+
+
+add $t3, $t2, $t1
+move $a0, $t3
+li $v0, 1
+syscall
+
+.end_macro
+
+
+.macro subtracao
+
+la $a0, num1
+li $v0, 4
+syscall
+li $v0, 5
+syscall
+move $t1, $v0
+
+la $a0, num2
+li $v0, 4
+syscall
+li $v0, 5
+syscall
+move $t2, $v0
+
+sub $t3, $t1, $t2
+move $a0, $t3
+li $v0, 1
+syscall
+
+.end_macro
+
+.macro multiplicar
+
+
+li $v0,4
+la $a0,num1
+syscall	
+li $v0,5
+syscall	
+
+move $t0, $v0	
+li $v0,4
+la $a0,num2
+syscall	
+
+li $v0,5
+
+syscall
+move $t1, $v0	
+mult $t0,$t1	
+mflo $s3	
+syscall
+
+.end_macro
+
 .text
-	
-	
-	
-	
-	
-	
-	
+.globl main 
+
+main:
+
 	#Imprime a mensagem presente na constante hello
 	li $v0, 4
 	la $a0, hello
@@ -23,6 +93,24 @@
 	syscall
 	#permite que o usuario atribua um valor a variavel v0
 	li $v0, 5
+	syscall
+	move $t4, $v0
+	beq $t4,3,multiplicacao
+	beq $t4,2,subtrair
+	beq $t4,1,soma
+	soma:
+	soma
+	li $v0, 10
+	syscall
+	
+	subtrair:
+	subtracao
+	li $v0,10
+	syscall
+	
+	multiplicacao:
+	multiplicar
+	li $v0,10
 	syscall
 	
 	
