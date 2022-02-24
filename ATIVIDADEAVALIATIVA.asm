74408c5ed9b514fc0f51e843221ab6d146ca56dc
Antônio Elvis Pinheiro <52458608+Elvis0032@users.noreply.github.com>
2022-02-24 18:59:42 -0300

    Update ATIVIDADEAVALIATIVA.asm

--- ATIVIDADEAVALIATIVA.asm
+++ ATIVIDADEAVALIATIVA.asm
@@ -58,22 +58,27 @@ syscall
 
 
 li $v0,4
-la $a0,num1
+la $a0,num2
+
 syscall	
 li $v0,5
 syscall	
 
 move $t0, $v0	
 li $v0,4
-la $a0,num2
+la $a0,num1
 syscall	
 
 li $v0,5
-
 syscall
+
 move $t1, $v0	
 mult $t0,$t1	
 mflo $s3	
+syscall
+
+li $v0,1
+move $a0,$s3	
 syscall
 
 .end_macro
