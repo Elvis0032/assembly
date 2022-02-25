a72c570bbcd641a3f486449de96be14cf7b25c1e
Antônio Elvis Pinheiro <52458608+Elvis0032@users.noreply.github.com>
2022-02-25 13:39:24 -0300

    teste de implementação

--- ATIVIDADEAVALIATIVA.asm
+++ ATIVIDADEAVALIATIVA.asm
@@ -4,6 +4,7 @@
 	num1: .asciiz "Informe o Primeiro Número: "
 	num2: .asciiz "Inoforme o Segundo Número: "
 	result: .asciiz "O resultado do calculo é: "
+	error_msg: .asciiz "Não é uma raiz quadrada perfeita"
 
 .macro soma 
 
@@ -74,7 +75,7 @@ syscall
 
 move $t1, $v0	
 mult $t0,$t1	
-mflo $s3	
+mfhi $s3	
 syscall
 
 li $v0,1
@@ -82,6 +83,81 @@ move $a0,$s3
 syscall
 
 .end_macro
+
+
+.macro dividir
+
+li $v0,4
+la $a0, num1
+syscall
+	
+li $v0,5
+syscall
+	
+move $t0, $v0
+	
+li $v0,4
+la $a0,num2
+syscall
+	
+li $v0,5
+syscall
+
+move $t1, $v0
+
+div $t0, $t1
+
+mflo $s3
+
+li $v0,1
+move $a0,$s3
+syscall
+
+.end_macro	
+
+.macro raiz
+		jal read_int		# le o inteiro
+		la 	$s0, ($v0)	# guarda o valor lido
+		li	$s1, 0
+		jal isqrt		# calcula a raiz quadrada		
+
+	print_int:
+		li $v0, 5	# código para ler um inteiro
+		syscall		# executa a chamada do SO para ler
+		jr $ra		# volta para o lugar de onde foi chamado (no caso, jal le_inteiro_do_teclado)			
+	
+	read_int:
+		li $v0, 5	# código para ler um inteiro
+		syscall		# executa a chamada do SO para ler
+		jr $ra		# volta para o lugar de onde foi chamado (no caso, jal le_inteiro_do_teclado)
+		
+	isqrt:
+		mul $t0, $s1, 2
+		add $t0, $t0, 1
+		sub $s0, $s0, $t0
+		add $s1, $s1, 1		# incrementa o contador, que sera o resultado da raiz
+		beq $s0, $zero, success	# se chegamos a zero a raiz é perfeita
+		slt	$t0, $s0, $zero	# caso seja menor que zero, deu problema
+		beq $t0, 1, error	# então mostramos mensagem de erro
+		j isqrt	# caso não ocorra nenhum dos casos acima, itera novamente
+		
+	error:
+		la $a0, error_msg
+		la $v0, 4
+		syscall
+		j	 exit
+	
+	success:
+		la $v0, 1
+		la $a0, ($s1)
+		syscall
+		j  exit
+	
+	exit:
+		la $v0, 10
+		syscall
+.end_macro
+
 
 .text
 .globl main 
@@ -100,9 +176,13 @@ main:
 	li $v0, 5
 	syscall
 	move $t4, $v0
-	beq $t4,3,multiplicacao
+	
+	beq $t4,5,raizquadrada
+	beq $t4,4,multiplicacao
+	beq $t4,3,dividir
 	beq $t4,2,subtrair
 	beq $t4,1,soma
+	
 	soma:
 	soma
 	li $v0, 10
@@ -113,8 +193,18 @@ main:
 	li $v0,10
 	syscall
 	
+	dividir:
+	dividir
+	li $v0,10
+	syscall
+	
 	multiplicacao:
 	multiplicar
+	li $v0,10
+	syscall
+	
+	raizquadrada:
+	raiz
 	li $v0,10
 	syscall
 	
